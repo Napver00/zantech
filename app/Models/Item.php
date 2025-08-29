ca05a8728d2f585cbbe8c806addae786455fb73d
Nakib00 <2022142@iub.edu.bd>
2025-08-30 03:46:27 +0600

    update items table in database add slug colounm

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -11,6 +11,7 @@ class Item extends Model
 
     protected $fillable = [
         'name',
+        'slug',
         'description',
         'short_description',
         'status',
--- database/migrations/2025_02_23_203147_create_items_table.php
+++ database/migrations/2025_02_23_203147_create_items_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('items', function (Blueprint $table) {
             $table->id();
+            $table->string('slug')->unique()->nullable();
             $table->string('name');
             $table->text('description');
             $table->text('short_description');
