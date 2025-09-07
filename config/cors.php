04c0e44df85dbd9c120188db7b53dd77fb809df9
Nakib00 <2022142@iub.edu.bd>
2025-09-08 00:51:06 +0600

    fdf

--- config/cors.php
+++ config/cors.php
@@ -1,21 +1,37 @@
 <?php
 
+// return [
+//     'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+//     'allowed_methods' => ['*'],
+
+//     'allowed_origins' => [
+//         'http://localhost:3000',
+//         'http://localhost:5173',
+//         'https://storeadmin.zantechbd.com',
+//     ],
+
+//     'allowed_headers' => ['*'],
+
+//     'exposed_headers' => [],
+
+//     'max_age' => 0,
+
+//     'supports_credentials' => true,
+// ];
+
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*'],  // allow all paths
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['*'], // allow all methods
 
-    'allowed_origins' => [
-        'http://localhost:3000',
-        'http://localhost:5173',
-        'https://storeadmin.zantechbd.com',
-    ],
+    'allowed_origins' => ['*'], // allow all origins
 
-    'allowed_headers' => ['*'],
+    'allowed_headers' => ['*'], // allow all headers
 
     'exposed_headers' => [],
 
     'max_age' => 0,
 
-    'supports_credentials' => true,
+    'supports_credentials' => true, // set false if you don’t need cookies/auth
 ];
