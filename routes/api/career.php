d9cffd8557c0ce7147c4f2fff36369d04522b852
Nakib00 <2022142@iub.edu.bd>
2025-09-10 00:28:43 +0600

    fad

--- routes/api/career.php
+++ routes/api/career.php
@@ -5,18 +5,19 @@ use App\Http\Controllers\Carrer\CarrerController;
 use App\Http\Controllers\Carrer\CareerFormController;
 
 
-
+Route::middleware('auth:sanctum')->group(function () {
+    Route::prefix('careers')->group(function () {
+        Route::get('/', [CarrerController::class, 'index']);
+        Route::get('/active', [CarrerController::class, 'activeCareers']);
+        Route::get('/{careers_id}', [CarrerController::class, 'show']);
+        Route::post('/', [CarrerController::class, 'store']);
+        Route::put('/{careers_id}', [CarrerController::class, 'update']);
+        Route::delete('/{careers_id}', [CarrerController::class, 'destroy']);
+        Route::patch('/status/{careers_id}', [CarrerController::class, 'changeStatus']);
+    });
+});
 Route::prefix('careers')->group(function () {
-    Route::get('/', [CarrerController::class, 'index']);
-    Route::get('/active', [CarrerController::class, 'activeCareers']);
-    Route::get('/{careers_id}', [CarrerController::class, 'show']);
-    Route::post('/', [CarrerController::class, 'store']);
-    Route::put('/{careers_id}', [CarrerController::class, 'update']);
-    Route::delete('/{careers_id}', [CarrerController::class, 'destroy']);
-    Route::patch('/status/{careers_id}', [CarrerController::class, 'changeStatus']);
-
     Route::post('/forms/{career_id}', [CareerFormController::class, 'store']);
     Route::get('/forms/{career_id}', [CareerFormController::class, 'index']);
     Route::get('/forms/{career_id}/{form_id}', [CareerFormController::class, 'show']);
-
 });
