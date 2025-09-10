9d48f6e48422f80a33d171908340a980567072ed
Nakib00 <2022142@iub.edu.bd>
2025-09-10 18:38:02 +0600

    fds

--- routes/api/career.php
+++ routes/api/career.php
@@ -5,6 +5,12 @@ use App\Http\Controllers\Carrer\CarrerController;
 use App\Http\Controllers\Carrer\CareerFormController;
 
 
+Route::prefix('careers')->group(function () {
+    Route::get('/active', [CarrerController::class, 'activeCareers']);
+    Route::post('/forms/{career_id}', [CareerFormController::class, 'store']);
+    Route::get('/forms/{career_id}', [CareerFormController::class, 'index']);
+});
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::prefix('careers')->group(function () {
         Route::get('/', [CarrerController::class, 'index']);
@@ -15,9 +21,4 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::patch('/status/{careers_id}', [CarrerController::class, 'changeStatus']);
         Route::get('/forms/{career_id}/{form_id}', [CareerFormController::class, 'show']);
     });
-});
-Route::prefix('careers')->group(function () {
-    Route::get('/active', [CarrerController::class, 'activeCareers']);
-    Route::post('/forms/{career_id}', [CareerFormController::class, 'store']);
-    Route::get('/forms/{career_id}', [CareerFormController::class, 'index']);
 });
