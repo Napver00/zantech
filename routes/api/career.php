dfd51d6d5c43b804ac26cedf3b61c0cd79aacf06
Nakib00 <2022142@iub.edu.bd>
2025-09-10 18:33:33 +0600

    fdfa

--- routes/api/career.php
+++ routes/api/career.php
@@ -13,11 +13,11 @@ Route::middleware('auth:sanctum')->group(function () {
         Route::put('/{careers_id}', [CarrerController::class, 'update']);
         Route::delete('/{careers_id}', [CarrerController::class, 'destroy']);
         Route::patch('/status/{careers_id}', [CarrerController::class, 'changeStatus']);
+        Route::get('/forms/{career_id}/{form_id}', [CareerFormController::class, 'show']);
     });
 });
 Route::prefix('careers')->group(function () {
     Route::get('/active', [CarrerController::class, 'activeCareers']);
     Route::post('/forms/{career_id}', [CareerFormController::class, 'store']);
     Route::get('/forms/{career_id}', [CareerFormController::class, 'index']);
-    Route::get('/forms/{career_id}/{form_id}', [CareerFormController::class, 'show']);
 });
